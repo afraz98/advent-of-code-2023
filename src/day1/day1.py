e6c180f6048fa2c8e07415aaf5d5fffe65c1704d
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-01 20:37:29 -0500

    Solution for day 1 part one

---
+++ src/day1/day1.py
@@ -0,0 +1,4 @@
+def solve_part_one(filename):
+    return sum(list(map(lambda x : (10*x[0]) + x[-1], list(map(lambda x : [int(i) for i in list(x) if i.isdigit()], [line.strip('\r\n') for line in open(filename, 'r')])))))
+
+print(solve_part_one('day1.txt'))
