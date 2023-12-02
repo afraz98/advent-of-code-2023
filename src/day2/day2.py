cf030265f985f416c412fadcca8652edd8469792
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-02 15:34:47 -0500

    Day two part one unoptimized solution

--- src/day2/day2.py
+++ src/day2/day2.py
@@ -1,11 +1,28 @@
 # Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. 
 # What is the sum of the IDs of those games?
  
+def parse_game(line):
+    game_id = line[0]
+    line = line[1:]
+    
+    blue_terms = 0
+    red_terms = 0
+    green_terms = 0
+
+    for term in range(0, len(line)-1):
+        if line[term + 1] == "blue":
+            if int(line[term]) > 14:
+                return (game_id, False)
+        if line[term + 1] == "red":
+            if int(line[term]) > 12:
+                return (game_id, False)
+        if line[term + 1] == "green":
+            if int(line[term]) > 13:
+                return (game_id, False)
+    return (game_id, True)
+
 def solve_part_one(filename):
-    red_cubes = 12
-    green_cubes = 13
-    blue_cubes = 14
-    lines = [line.strip("\r\n").split(" ") for line in open(filename, "r")]
-    print(lines)
+    return sum(list(map(lambda game: int(game[0]) if game[1] else 0, list(map(lambda x: parse_game(x), [line.strip("\r\n").replace(":","").replace(",", "").replace(";", "").split(" ")[1:] for line in open(filename, "r")])))))
+    
     
 print(solve_part_one("day2.txt"))
