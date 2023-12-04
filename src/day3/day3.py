c7ce0150b80b7893fd403ce883dc75d1397fe6f3
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-03 23:18:53 -0500

    Find symbols in input

--- src/day3/day3.py
+++ src/day3/day3.py
@@ -1,5 +1,18 @@
-def solve_part_one(filename):
-    input = [[char for char in line.strip("\r\n")] for line in open(filename, 'r')]
-    print(input)
+def find_symbols(map):
+    symbols = []
+    for row in range(0, len(map)):
+        for column in range(0, len(map[0])):
+            if map[row][column] not in "0123456789" and map[row][column] != ".":
+                symbols.append((map[row][column], row, column))
+    return symbols
 
-solve_part_one("day3_test.txt")
+def find_part_numbers(map):
+    pass
+
+def solve_part_one(filename):
+    map = [[char for char in line.strip("\r\n")] for line in open(filename, 'r')]
+    symbols = find_symbols(map)
+    print(symbols)
+    return -1
+
+print(solve_part_one("day3.txt"))
