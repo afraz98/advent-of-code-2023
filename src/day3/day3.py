dac68d8c7fb88948e96380a89787123ca98e8b41
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-03 23:58:49 -0500

    TODO: Filter part numbers such that it only contains numbers with an adjacent symbol

--- src/day3/day3.py
+++ src/day3/day3.py
@@ -7,12 +7,22 @@ def find_symbols(map):
     return symbols
 
 def find_part_numbers(map):
-    pass
+    numbers = []
+    number = ""
+    for row in range(0, len(map)):
+        for column in range(0, len(map[0])):
+            if map[row][column] in "0123456789":
+                number += map[row][column]
+            if(map[row][column] == "."):
+                numbers.append(number)
+                number = ""
+    return list(filter(lambda x: x != '', [number if number != '' else '' for number in numbers]))
+
+def find_numbers_with_symbols_adjacent(part_numbers, symbols):
+    return []
 
 def solve_part_one(filename):
     map = [[char for char in line.strip("\r\n")] for line in open(filename, 'r')]
-    symbols = find_symbols(map)
-    print(symbols)
-    return -1
+    return sum([int(part_number) for part_number in find_numbers_with_symbols_adjacent(find_part_numbers(map), find_symbols(map))])
 
 print(solve_part_one("day3.txt"))
