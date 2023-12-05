fa969e9a20191191d522feaeea5e632dd21b07ec
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-04 21:07:44 -0500

    Day 4 part one unoptimized

--- src/day4/day4.py
+++ src/day4/day4.py
@@ -1,7 +1,24 @@
-def solve_part_one(filename):
-    input = [line.strip("\r\n").replace(":", "").split(" ")[1:] for line in open(filename, 'r')]
-    
-    for game in input:
-        game_id = game[0]
+def tally_points(games):
+    sum = 0
+    for game in games:
+        points = 0
         
+        game = list(filter(lambda x: x != '', game))[2:]
+        winning_numbers, actual_numbers = ":".join(game).split("|")
+
+        winning_numbers = list(filter(lambda x: x != '', winning_numbers.split(":")))
+        actual_numbers = list(filter(lambda x: x != '', actual_numbers.split(":")))
+
+        for number in actual_numbers:
+            if number in winning_numbers:
+                if points == 0:
+                    points = 1
+                else:
+                    points *= 2
+        sum += points
+    return sum
+
+def solve_part_one(filename):
+    return tally_points([line.strip("\t\r\n").replace(":", "").split(" ") for line in open(filename, 'r')])
+
 print(solve_part_one("day4.txt"))
