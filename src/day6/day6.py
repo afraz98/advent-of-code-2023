7c70c726cc4b095a4a2e4e24ddaabfffc7264ed8
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-07 00:01:10 -0500

    Day 6 part two brute force

--- src/day6/day6.py
+++ src/day6/day6.py
@@ -1,11 +1,12 @@
 from functools import reduce
 
+# Maximize acceleration such that [t_acceleration] + [t_max - t_acceleration]x > d_record
 def find_methods_to_beat_record(races):
     methods_to_beat_record = []
     for race in races:
         methods = 0
         for time in range(int(race[0])):
-            if(int(race[0]) - time) * (time) > int(race[1]):
+            if(int(race[0]) - time) * (time) - int(race[1]) > 0:
                 methods += 1
         methods_to_beat_record.append(methods)
     return reduce(lambda x, y: x * y, methods_to_beat_record)
@@ -24,4 +25,17 @@ def solve_part_one(filename):
     races = list(zip(times, distances))
     return find_methods_to_beat_record(races)
 
+def solve_part_two(filename):
+    input = [line.strip('\r\n').split(" ")[1:] for line in open(filename, "r")]
+    for line in input:
+        line = filter(lambda x: x != '', [x for x in line])
+    
+    time = ''.join(list(filter(lambda x: x!= '', input[0])))
+    distance = ''.join(list(filter(lambda x: x!='', input[1])))
+
+    print(int(time), int(distance))
+
+    return find_methods_to_beat_record([(int(time), int(distance))])
+
 print(solve_part_one("day6.txt"))
+print(solve_part_two("day6.txt"))
