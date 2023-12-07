b2f8ad4794c7794d7125c5d2a324e99e1fb9ac29
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-06 21:22:09 -0500

    Duplicate cards should NOT be processed as individual, new cards. Instead, multiply values successively

--- src/day4/day4.py
+++ src/day4/day4.py
@@ -19,37 +19,29 @@ def tally_points(games):
     return sum
 
 def tally_cards(games):
-    number_of_cards = 0
-    active_games = [game for game in games]
+    card_multipliers = [1 for i in range(0, len(games))]
+    
+    for game in games:
+        print("Game %s starting" % (str(games.index(game))))
+        matches = 0
 
-    while active_games != []:
-        points = 0
-        index = 0
-
-        active_game = list(filter(lambda x: x != '', active_games[0]))
-        winning_numbers, actual_numbers = ":".join(active_game[2:]).split("|")
+        multiplier = card_multipliers[games.index(game)]
+        winning_numbers, actual_numbers = ":".join(game[2:]).split("|")
 
         winning_numbers = list(filter(lambda x: x != '', winning_numbers.split(":")))
         actual_numbers = list(filter(lambda x: x != '', actual_numbers.split(":")))
 
         for number in actual_numbers:
             if number in winning_numbers:
-                points += 1
+                matches += 1
 
-        print(active_game[0], active_game[1], winning_numbers, actual_numbers, points)
+        game_index = games.index(game)
+        print("Game %s had %s matches" % (str(game_index), str(matches)))
 
-        # Find original card that is the (index + i) index after the active game's id
-        for game in games:
-            if game[1] == active_game[1]: # Same game IDs
-                index = games.index(game)
+        for i in range(game_index + 1, game_index + matches + 1):
+            card_multipliers[i] += multiplier
 
-        for i in range(1, points + 1):
-            active_games.append(games[index + i])
-
-        active_games.pop(0)
-        number_of_cards += 1
-
-    return number_of_cards
+    return sum(card_multipliers)
 
 def solve_part_one(filename):
     return tally_points([line.strip("\r\n").replace(":", "").split(" ") for line in open(filename, 'r')])
