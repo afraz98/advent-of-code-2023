f40646e9456877eb45730629e951e548bc28e66f
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-07 22:05:55 -0500

    Day 7 part one solution

--- src/day7/day7.py
+++ src/day7/day7.py
@@ -36,7 +36,7 @@ def is_two_pair(cards):
         if cards[i] == cards[i+1]:
             temp = [j for j in temp if j != temp[i]]
             for i in range(0, 2):
-                if cards[i] == cards[i+1]:
+                if temp[i] == temp[i+1]:
                     return True
     return False
 
@@ -47,26 +47,21 @@ def is_pair(cards):
     return False
 
 def rank(hand):
-    cards, bet = hand
+    cards, bets = hand
+    
     # Sort each hand by lexicographical order before sorting by class
     cards = ''.join(sorted(cards, key=order))
     
-    
     if is_five_of_a_kind(cards):
         return 6
-    
     if is_four_of_a_kind(cards):
         return 5
-
     if is_full_house(cards):
         return 4
-
     if is_three_of_a_kind(cards):
         return 3
-    
     if is_two_pair(cards):
         return 2
-
     if is_pair(cards):
         return 1
 
@@ -79,9 +74,8 @@ def solve_part_one(filename):
     # Sort by the class of the hand. 
     # Five of a kind > Four of a kind > Full house > Three of a kind > Two pair > One Pair > High Card
     sorted_hands = sorted(input, key=lambda x: (rank(x), [card_ranking.index(c) for c in x[0]]))
-    print(sorted_hands)
-
+    
     # Sort by the rank of each card
     return sum([(int(sorted_hands.index(j))+1)*int(j[1]) for j in sorted_hands])
 
-print(solve_part_one("day7_test.txt"))
+print(solve_part_one("day7.txt"))
