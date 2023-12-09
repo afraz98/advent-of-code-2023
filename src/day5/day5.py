a4a2b7afc7e207c786d8d2ae35af0f41e483a8b6
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-08 20:50:03 -0500

    Day 5 part one working example input

--- src/day5/day5.py
+++ src/day5/day5.py
@@ -1,7 +1,62 @@
-def solve_part_one(filename):
-    input = [line.strip() for line in open(filename, "r") if line != "\n"]
-    print(input)
-    seeds = input[0].split(": ")[1].split(" ")
-    pass
+def convert_value(value, mappings):
+    for sources, destinations in mappings:
+        if value in sources:
+            return destinations[sources.index(value)]
+    return value
 
-print(solve_part_one("day5.txt"))
+def parse_next_map(input):
+    mappings = []
+    while(input[0] != "\n"):
+        dest_start, src_start, step = [int(number) for number in input[0].strip("\n").split(" ")]
+        mappings.append((list(range(src_start, src_start + step)), list(range(dest_start, dest_start + step))))
+        input = input[1:]
+    return mappings, input
+
+def solve_part_one(filename):
+    input = [line for line in open(filename, "r")]
+    
+    # Process the list of seeds
+    values = [int(seed) for seed in input[0].strip("\n").split(": ")[1].split(" ")]
+    input = input[1:]
+
+    mappings = []   
+    input = input[2:]
+
+    # Process seed to soil map
+    mappings, input = parse_next_map(input)
+    values = [convert_value(seed, mappings) for seed in values]
+    input = input[2:]
+
+    # Process soil to fertilizer map
+    mappings, input = parse_next_map(input)
+    values = [convert_value(soil, mappings) for soil in values]
+    input = input[2:]
+
+    # Process fertilizer to water map
+    mappings, input = parse_next_map(input)
+    values = [convert_value(fertilizer, mappings) for fertilizer in values]
+    input = input[2:]
+
+    # Process water to light map
+    mappings, input = parse_next_map(input)
+    values = [convert_value(water, mappings) for water in values]
+    input = input[2:]
+
+    # Process light to temperature map
+    mappings, input = parse_next_map(input)
+    values = [convert_value(light, mappings) for light in values]
+    input = input[2:]
+
+    # Process temperature to humidity map
+    mappings, input = parse_next_map(input)
+    values = [convert_value(temperature, mappings) for temperature in values]
+    input = input[2:]
+
+    # Process humidity to location map
+    mappings, input = parse_next_map(input)
+    values = [convert_value(humidity, mappings) for humidity in values]
+    input = input[2:]
+
+    return min(values)
+
+print(solve_part_one("day5_test.txt"))
