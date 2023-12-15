093e97e12c1ac5f88778a13fd71d703281ce6ffe
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-14 21:28:21 -0500

    Day 11 part one and two solutions.

--- src/day11/day11.py
+++ src/day11/day11.py
@@ -1,4 +1,4 @@
-def solve_part_one(filename):
+def solve(filename, factor):
     image = [[char for char in line.strip('\n')] for line in open(filename, 'r')]
     
     empty_rows = set()
@@ -23,17 +23,25 @@ def solve_part_one(filename):
             if image[row][column] == '#':
                 galaxies.add((row,column))
     distances = []
-    
+
     for galaxy in galaxies:
         for other_galaxy in galaxies - set([galaxy]):
             distance = abs(galaxy[0] - other_galaxy[0]) + abs(galaxy[1] - other_galaxy[1])
             for row in empty_rows:
                 if galaxy[0] < row < other_galaxy[0] or other_galaxy[0] < row < galaxy[0]:
-                    distance += 1
+                    distance += (factor - 1)
             for col in empty_cols:
                 if galaxy[1] < col < other_galaxy[1] or other_galaxy[1] < col < galaxy[1]:
-                    distance += 1
+                    distance += (factor - 1)
             distances.append(distance)
-    return sum(distances)
+    return sum(distances) // 2
+
+
+def solve_part_one(filename):
+    return solve(filename, 2)
+
+def solve_part_two(filename):
+    return solve(filename, 1000000)
 
 print(solve_part_one("day11.txt"))
+print(solve_part_two("day11.txt"))
