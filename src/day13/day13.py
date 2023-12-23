046382b7a8ffc49fe2e0ece5f116719a014d323e
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-22 22:06:35 -0500

    Now ACTUALLY passing the examle case

--- src/day13/day13.py
+++ src/day13/day13.py
@@ -1,27 +1,24 @@
 def parse_input(filename):
-    return [[c for c in line.strip('\n')] for line in open(filename, 'r')]
+    return [[row for row in map.split('\n')] for map in open(filename, 'r').read().split('\n\n')]
 
 def find_reflection_rows(map):
     for i in range(1, len(map)):
         if map[i] == map[i-1]:
-            if all([map[i+j] == map[i-1-j] for j in range(i-len(map))]):
-                return i+1
+            if all([map[i+j] == map[i-1-j] for j in range(len(map)-i)]):
+                print("Row %d" % (i))
+                return i
     return 0
 
 def find_reflection_columns(map):
     for i in range(1, len(map[0])):
         if [map[x][i-1] for x in range(len(map))] == [map[x][i] for x in range(len(map))]:
-            print(i)
-            if all([[map[x][i-1+j] for x in range(len(map))] == [map[x][i-j] for x in range(len(map))] for j in range(i-len(map[0]))]):
+            if all([[map[x][i-1-j] for x in range(len(map))] == [map[x][i+j] for x in range(len(map))] for j in range(len(map[0])-i)]):
+                print("Column %d" % i)
                 return i
     return 0
 
 def solve_part_one(filename):
-    map = parse_input(filename)
-    return 100 * find_reflection_rows(map) + find_reflection_columns(map)
-
-def solve_part_two(filename):
-    pass
+    return sum([(100*find_reflection_rows(map)) + find_reflection_columns(map) for map in parse_input(filename)])
 
 print(solve_part_one("day13_test.txt"))
 
