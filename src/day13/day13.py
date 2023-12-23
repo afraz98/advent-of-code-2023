9bcc0f04e47a59adbab1202c4cff06119fbf8d28
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-22 21:05:46 -0500

    Day 13 part one working for example

--- src/day13/day13.py
+++ src/day13/day13.py
@@ -3,14 +3,17 @@ def parse_input(filename):
 
 def find_reflection_rows(map):
     for i in range(1, len(map)):
-        if all([map[i+j] == map[i-1+j] for j in range(i-len(map))]):
-            return i - 1
+        if map[i] == map[i-1]:
+            if all([map[i+j] == map[i-1-j] for j in range(i-len(map))]):
+                return i+1
     return 0
 
 def find_reflection_columns(map):
     for i in range(1, len(map[0])):
-        if all([[map[x][i-1+j] for x in range(len(map))] == [map[x][i+j] for x in range(len(map))] for j in range(i-len(map[0]))]):
-            i - 1
+        if [map[x][i-1] for x in range(len(map))] == [map[x][i] for x in range(len(map))]:
+            print(i)
+            if all([[map[x][i-1+j] for x in range(len(map))] == [map[x][i-j] for x in range(len(map))] for j in range(i-len(map[0]))]):
+                return i
     return 0
 
 def solve_part_one(filename):
