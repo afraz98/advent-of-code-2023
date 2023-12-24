c28ef22eee287e4130888abd9f7398b3680dbd43
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-24 16:01:44 -0500

    Day 13 part two passing example data

--- src/day13/README.md
+++ src/day13/README.md
@@ -78,6 +78,7 @@ In each pattern, you'll need to locate and fix the smudge that causes a differen
 
 Here's the above example again:
 
+```
 #.##..##.
 ..#.##.#.
 ##......#
@@ -93,8 +94,11 @@ Here's the above example again:
 #####.##.
 ..##..###
 #....#..#
+```
+
 The first pattern's smudge is in the top-left corner. If the top-left # were instead ., it would have a different, horizontal line of reflection:
 
+```
 1 ..##..##. 1
 2 ..#.##.#. 2
 3v##......#v3
@@ -102,10 +106,13 @@ The first pattern's smudge is in the top-left corner. If the top-left # were ins
 5 ..#.##.#. 5
 6 ..##..##. 6
 7 #.#.##.#. 7
+```
+
 With the smudge in the top-left corner repaired, a new horizontal line of reflection between rows 3 and 4 now exists. Row 7 has no corresponding reflected row and can be ignored, but every other row matches exactly: row 1 matches row 6, row 2 matches row 5, and row 3 matches row 4.
 
 In the second pattern, the smudge can be fixed by changing the fifth symbol on row 2 from . to #:
 
+```
 1v#...##..#v1
 2^#...##..#^2
 3 ..##..### 3
@@ -113,6 +120,8 @@ In the second pattern, the smudge can be fixed by changing the fifth symbol on r
 5 #####.##. 5
 6 ..##..### 6
 7 #....#..# 7
+```
+
 Now, the pattern has a different horizontal line of reflection between rows 1 and 2.
 
 Summarize your notes as before, but instead use the new different reflection lines. In this example, the first pattern's new horizontal line has 3 rows above it and the second pattern's new horizontal line has 1 row above it, summarizing to the value 400.
--- src/day13/day13.py
+++ src/day13/day13.py
@@ -1,28 +1,56 @@
 def parse_input(filename):
-    return [[row for row in map.split('\n')] for map in open(filename, 'r').read().split('\n\n')]
+    return [[[character for character in row] for row in map.split('\n')] for map in open(filename, 'r').read().split('\n\n')]
+
+def count_differences(str1, str2):
+    # From here: https://stackoverflow.com/questions/28423448/counting-differences-between-two-strings
+    return sum(1 for a, b in zip(str1, str2) if a != b)
 
 def find_reflections(map):
-    for row in map:
-        print(row)
-
     # First check horizontal lines (row splits)
     for i in range(1, len(map)):
         if all([map[i+j] == map[i-1-j] for j in range(min(len(map)-i, i))]):
-            print("Row %d\n" % i)
             return 100 * i
 
     # Next check vertical lines (column splits)
     for i in range(1, len(map[0])):
         if all([[map[x][i-1-j] for x in range(len(map))] == [map[x][i+j] for x in range(len(map))] for j in range(min(len(map[0])-i, i))]):
-            print("Column %d\n" % i)
             return i
-
-    # No reflections found
-    print()
     return 0
 
 def solve_part_one(filename):
     return sum(find_reflections(map) for map in parse_input(filename))
 
-print(solve_part_one("day13.txt"))
+def fix_smudges(maps):
+    for map in maps:
+        # First check horizontal lines (row splits)
+        for i in range(1, len(map)):
+            if count_differences(map[i-1], map[i]) == 1:
+                map[i] = map[i-1]
 
+        for i in range(1, len(map)):
+            for j in range(min(len(map)-i, i)):
+                if count_differences(map[i+j], map[i-1-j]) == 1:
+                        map[i+j] = map[i-1-j]
+
+        # Next check vertical lines (column) splits
+        for i in range(1, len(map[0])):
+            if count_differences([map[x][i-1] for x in range(len(map))], [map[x][i] for x in range(len(map))]) == 1:
+                for x in range(len(map)):
+                    map[x][i-1] = map[x][i]
+
+        for i in range(1, len(map[0])):
+            for j in range(min(len(map[0])-i, i)):
+                if count_differences([map[x][i-1-j] for x in range(len(map))], [map[x][i+j] for x in range(len(map))]) == 1:
+                    for x in range(len(map)):
+                        map[x][i-1-j] = map[x][i+j]
+
+
+    return maps
+
+# 37813?
+# 37282?
+def solve_part_two(filename):
+    return sum(find_reflections(map) for map in fix_smudges(parse_input(filename)))
+
+print(solve_part_one("day13.txt"))
+print(solve_part_two("day13.txt"))
