c028698f2893baca8636613a073aa6da3e06cd3d
afraz98 <anthonymfrazier1998@gmail.com>
2023-12-23 23:37:21 -0500

    Day 13 part one solution

--- src/day13/day13.py
+++ src/day13/day13.py
@@ -2,17 +2,23 @@ def parse_input(filename):
     return [[row for row in map.split('\n')] for map in open(filename, 'r').read().split('\n\n')]
 
 def find_reflections(map):
+    for row in map:
+        print(row)
+
     # First check horizontal lines (row splits)
     for i in range(1, len(map)):
-        if all([map[i+j] == map[i-1-j] for j in range(len(map)-i)]):
+        if all([map[i+j] == map[i-1-j] for j in range(min(len(map)-i, i))]):
+            print("Row %d\n" % i)
             return 100 * i
 
     # Next check vertical lines (column splits)
     for i in range(1, len(map[0])):
-        if all([[map[x][i-1-j] for x in range(len(map))] == [map[x][i+j] for x in range(len(map))] for j in range(len(map[0])-i)]):
+        if all([[map[x][i-1-j] for x in range(len(map))] == [map[x][i+j] for x in range(len(map))] for j in range(min(len(map[0])-i, i))]):
+            print("Column %d\n" % i)
             return i
-            
+
     # No reflections found
+    print()
     return 0
 
 def solve_part_one(filename):
